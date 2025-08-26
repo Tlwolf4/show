95c281eccb4f2af36f2e36603cccde9073b3ec00
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2025-08-26 14:00:55 +0000

    modificado script

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let idade = parseInt(prompt("Quantos anos você tem:"))
 let ingreso = prompt("Voce tem ingreso? [S/N]").toLowerCase() // true
 
 if (ingresso === "s") {
-    ingrsso = true
+   
   } if (idade >= 18 && ingresso == true) {
     alert("Você pode entrar!")
   } else {
     alert("Você não pode entrar...")
 
-} else (ingresso === "n") {
-    ingresso = false
+} if (ingresso === "n") {
+    
 } else {
     alert("você digitou algo errado. Fim do programa.")
 }
